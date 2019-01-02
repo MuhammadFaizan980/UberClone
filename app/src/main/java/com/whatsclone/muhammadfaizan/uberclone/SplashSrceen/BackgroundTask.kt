6691d1ba3cdf4c09caf1af57e6612e1d1d98366e
Faizan <mohammadfaizan980@gmail.com>
2019-01-02 16:57:02 +0500

    Converted background task code to kotlin

---
+++ app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/SplashSrceen/BackgroundTask.kt
@@ -0,0 +1,35 @@
+package com.whatsclone.muhammadfaizan.uberclone.SplashSrceen
+
+import android.content.Context
+import android.content.Intent
+import android.os.AsyncTask
+import android.widget.ProgressBar
+
+import com.whatsclone.muhammadfaizan.uberclone.LoginRegister.LoginActivity
+
+class BackgroundTask constructor(progressBar: ProgressBar, context: Context) : AsyncTask<Void, Int, Int>() {
+
+    private var progressBar : ProgressBar = progressBar
+    private var context : Context = context
+
+    override fun doInBackground(vararg voids: Void): Int? {
+        for (i in 1..100) {
+            try {
+                Thread.sleep(100)
+                publishProgress(i)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+
+        }
+        return null
+    }
+
+    override fun onProgressUpdate(vararg values: Int?) {
+        progressBar.progress = values[0]!!
+    }
+
+    override fun onPostExecute(integer: Int?) {
+        context.startActivity(Intent(context.applicationContext, LoginActivity::class.java))
+    }
+}
