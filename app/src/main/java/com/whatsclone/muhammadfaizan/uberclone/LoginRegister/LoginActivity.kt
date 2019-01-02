bb21a2d7d7bf14f66547c23edf4eb932622b62c7
Faizan <mohammadfaizan980@gmail.com>
2019-01-02 16:50:25 +0500

    Added login activity

---
+++ app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/LoginRegister/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.whatsclone.muhammadfaizan.uberclone.LoginRegister
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.whatsclone.muhammadfaizan.uberclone.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/SplashSrceen/SplashScreenActivity.kt
+++ app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/SplashSrceen/SplashScreenActivity.kt
@@ -11,6 +11,6 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
     }
 }
