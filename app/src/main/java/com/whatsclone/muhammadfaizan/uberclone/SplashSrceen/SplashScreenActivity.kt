4e32acbbf67361ef0d5a334deacd84014a912e9b
Faizan <mohammadfaizan980@gmail.com>
2019-01-02 16:42:28 +0500

    Added splash screen design

---
+++ app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/SplashSrceen/SplashScreenActivity.kt
@@ -0,0 +1,16 @@
+package com.whatsclone.muhammadfaizan.uberclone.SplashSrceen
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+import com.whatsclone.muhammadfaizan.uberclone.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main)
+    }
+}
