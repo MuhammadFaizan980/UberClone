0411f5a58dde127730c7bd6b1c86888ce7f27111
Faizan <mohammadfaizan980@gmail.com>
2019-01-04 16:57:19 +0500

    Added welcome screen kotlin code

--- app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/DriverComponents/LoginRegister/View/LoginViewDriver.kt
+++ app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/DriverComponents/LoginRegister/View/LoginViewDriver.kt
@@ -1,13 +1,13 @@
-package com.whatsclone.muhammadfaizan.uberclone.LoginRegister
+package com.whatsclone.muhammadfaizan.uberclone.DriverComponents.LoginRegister.View
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.whatsclone.muhammadfaizan.uberclone.R
 
-class LoginView : AppCompatActivity() {
+class LoginViewDriver : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_view)
+        setContentView(R.layout.activity_login_view_driver)
     }
 }
--- app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/WelcomeActivity/ActivityWelcome.kt
+++ app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/WelcomeActivity/ActivityWelcome.kt
@@ -1,16 +1,42 @@
 package com.whatsclone.muhammadfaizan.uberclone.WelcomeActivity
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.WindowManager
+import android.widget.Button
+import com.whatsclone.muhammadfaizan.uberclone.DriverComponents.LoginRegister.View.LoginViewDriver
 import com.whatsclone.muhammadfaizan.uberclone.R
+import com.whatsclone.muhammadfaizan.uberclone.UserComponents.LoginRegister.LoginViewRider
 
 class ActivityWelcome : AppCompatActivity() {
+
+    private lateinit var btnDriverLogin: Button
+    private lateinit var btnRiderLogin: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_welcome)
+        initViews()
+        setListeners()
+    }
+
+    private fun initViews() {
+        btnDriverLogin = findViewById(R.id.btnLoginDriver)
+        btnRiderLogin = findViewById(R.id.btnLoginRider)
+    }
+
+    private fun setListeners() {
+        btnRiderLogin.setOnClickListener {
+            startActivity(Intent(this@ActivityWelcome, LoginViewRider::class.java))
+            this@ActivityWelcome.finish()
+        }
+
+        btnDriverLogin.setOnClickListener {
+            startActivity(Intent(this@ActivityWelcome, LoginViewDriver::class.java))
+            this@ActivityWelcome.finish()
+        }
     }
 }
