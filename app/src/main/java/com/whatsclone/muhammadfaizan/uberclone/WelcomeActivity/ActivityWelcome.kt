0f24c5139607acf5ea756e7cf9ee4498149fc158
Faizan <mohammadfaizan980@gmail.com>
2019-01-04 16:22:30 +0500

    Added welcome screen

---
+++ app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/WelcomeActivity/ActivityWelcome.kt
@@ -0,0 +1,13 @@
+package com.whatsclone.muhammadfaizan.uberclone.WelcomeActivity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.whatsclone.muhammadfaizan.uberclone.R
+
+class ActivityWelcome : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
