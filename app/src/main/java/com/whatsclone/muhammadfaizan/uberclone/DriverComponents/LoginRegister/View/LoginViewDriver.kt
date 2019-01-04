65e229c62957903c84ccfa2afc12e9588716f64e
Faizan <mohammadfaizan980@gmail.com>
2019-01-04 17:03:14 +0500

    Added go back code

--- app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/DriverComponents/LoginRegister/View/LoginViewDriver.kt
+++ app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/DriverComponents/LoginRegister/View/LoginViewDriver.kt
@@ -1,13 +1,20 @@
 package com.whatsclone.muhammadfaizan.uberclone.DriverComponents.LoginRegister.View
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.whatsclone.muhammadfaizan.uberclone.R
+import com.whatsclone.muhammadfaizan.uberclone.WelcomeActivity.ActivityWelcome
 
 class LoginViewDriver : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_view_driver)
+    }
+
+    override fun onBackPressed() {
+        startActivity(Intent(this@LoginViewDriver, ActivityWelcome::class.java))
+        this@LoginViewDriver.finish()
     }
 }
--- app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/RiderComponents/LoginRegister/View/LoginViewRider.kt
+++ app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/RiderComponents/LoginRegister/View/LoginViewRider.kt
@@ -1,13 +1,20 @@
 package com.whatsclone.muhammadfaizan.uberclone.UserComponents.LoginRegister
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.whatsclone.muhammadfaizan.uberclone.R
+import com.whatsclone.muhammadfaizan.uberclone.WelcomeActivity.ActivityWelcome
 
 class LoginViewRider : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_view_rider)
+    }
+
+    override fun onBackPressed() {
+        startActivity(Intent(this@LoginViewRider, ActivityWelcome::class.java))
+        this@LoginViewRider.finish()
     }
 }
