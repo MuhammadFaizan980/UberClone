0a6ad497c8fea186eac46375ab18ad79811eef7f
Faizan <mohammadfaizan980@gmail.com>
2019-01-05 15:58:30 +0500

    Added firebase login verification an, progress bar and also error dialog

--- app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/RiderComponents/LoginRegister/View/LoginViewRider.kt
+++ app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/RiderComponents/LoginRegister/View/LoginViewRider.kt
@@ -1,16 +1,19 @@
 package com.whatsclone.muhammadfaizan.uberclone.UserComponents.LoginRegister
 
 import android.annotation.SuppressLint
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
 import android.support.design.widget.Snackbar
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
 import com.whatsclone.muhammadfaizan.uberclone.R
 import com.whatsclone.muhammadfaizan.uberclone.RiderComponents.LoginRegister.Presenter.IPresenterRider
@@ -24,6 +27,7 @@ class LoginViewRider : AppCompatActivity(), ILoginViewRider {
     private lateinit var edtEmail: EditText
     private lateinit var edtPassword: EditText
     private lateinit var btnLogin: Button
+    private lateinit var progressbar: ProgressBar
     private lateinit var presenterRider: IPresenterRider
     private var email: String = ""
     private var password: String = ""
@@ -40,12 +44,14 @@ class LoginViewRider : AppCompatActivity(), ILoginViewRider {
         edtEmail = findViewById(R.id.edt_email_login_rider)
         edtPassword = findViewById(R.id.edt_password_login_rider)
         btnLogin = findViewById(R.id.btn_login_rider)
+        progressbar = findViewById(R.id.progress_login_rider)
         presenterRider = PresenterRider(this@LoginViewRider)
     }
 
     private fun passListenerToPresenter() {
         btnLogin.setOnClickListener {
             btnLogin.isEnabled = false
+            progressbar.visibility = View.VISIBLE
             email = edtEmail.text.toString()
             password = edtPassword.text.toString()
             presenterRider.onLoginInitiated(email, password)
@@ -62,19 +68,19 @@ class LoginViewRider : AppCompatActivity(), ILoginViewRider {
         if (results) {
             presenterRider.authenticateUser(email, password)
         } else {
+            hideProgressBar()
             var snackBar: Snackbar = Snackbar.make(container, "Enter a valid email address and password", Snackbar.LENGTH_SHORT)
             var mView: View = snackBar.view
             mView.setBackgroundColor(ContextCompat.getColor(this@LoginViewRider, R.color.red))
             var txtView: TextView = mView.findViewById(android.support.design.R.id.snackbar_text) as TextView
             txtView.setTextColor(Color.WHITE)
             snackBar.show()
-            btnLogin.isEnabled = true
         }
     }
 
     override fun onFirebaseResults(results: Boolean, exc: Exception?) {
         if (results) {
-            btnLogin.isEnabled = true
+            hideProgressBar()
             var snackBar: Snackbar = Snackbar.make(container, "Log in success", Snackbar.LENGTH_SHORT)
             var mView: View = snackBar.view
             mView.setBackgroundColor(ContextCompat.getColor(this@LoginViewRider, R.color.blue))
@@ -82,16 +88,33 @@ class LoginViewRider : AppCompatActivity(), ILoginViewRider {
             txtView.setTextColor(Color.WHITE)
             snackBar.show()
         } else {
-            btnLogin.isEnabled = true
+            hideProgressBar()
             var snackBar: Snackbar = Snackbar.make(container, "Log in error", Snackbar.LENGTH_LONG)
-            snackBar.setAction("DETAILS", View.OnClickListener {
-                //TODO: show error dialog with details
-            })
+            snackBar.setAction("DETAILS") {
+                var dialog: AlertDialog.Builder = AlertDialog.Builder(this@LoginViewRider, R.style.mDialog)
+                dialog.setTitle("Login Error")
+                dialog.setCancelable(false)
+                dialog.setMessage(exc!!.message!!.toString())
+                dialog.setPositiveButton("Close") { alertDialog, which ->
+                    alertDialog.cancel()
+                }
+                dialog.show()
+            }
             var mView: View = snackBar.view
             mView.setBackgroundColor(ContextCompat.getColor(this@LoginViewRider, R.color.red))
             var txtView: TextView = mView.findViewById(android.support.design.R.id.snackbar_text) as TextView
             txtView.setTextColor(Color.WHITE)
             snackBar.show()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        progressbar.visibility = View.INVISIBLE
+    }
+
+    private fun hideProgressBar() {
+        this.progressbar.visibility = View.INVISIBLE
+        btnLogin.isEnabled = true
     }
 }
