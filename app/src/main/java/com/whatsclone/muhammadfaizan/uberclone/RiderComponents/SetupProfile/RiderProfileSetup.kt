210ea38a1391fc68e405a1db4837260498b46459
Faizan <mohammadfaizan980@gmail.com>
2019-01-08 16:48:52 +0500

    Added rider profile setup + UI + Added picasso and circle imageview dependencies

---
+++ app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/RiderComponents/SetupProfile/RiderProfileSetup.kt
@@ -0,0 +1,13 @@
+package com.whatsclone.muhammadfaizan.uberclone.RiderComponents.SetupProfile
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.whatsclone.muhammadfaizan.uberclone.R
+
+class RiderProfileSetup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rider_profile_setup)
+    }
+}
