0c3795ed0e4a54dc59d0d2d98cbdbcc6fd756a3b
Faizan <mohammadfaizan980@gmail.com>
2019-01-08 17:33:59 +0500

    Added code to get user image from gallery and put it into a stream

--- app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/RiderComponents/SetupProfile/ProfileSetupView/RiderProfileSetup.kt
+++ app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/RiderComponents/SetupProfile/ProfileSetupView/RiderProfileSetup.kt
@@ -1,6 +1,10 @@
 package com.whatsclone.muhammadfaizan.uberclone.RiderComponents.SetupProfile.ProfileSetupView
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
@@ -10,6 +14,7 @@ import com.whatsclone.muhammadfaizan.uberclone.R
 import com.whatsclone.muhammadfaizan.uberclone.RiderComponents.SetupProfile.ProfileSetupPresenter.IRiderProfileSetupPresenter
 import com.whatsclone.muhammadfaizan.uberclone.RiderComponents.SetupProfile.ProfileSetupPresenter.RiderProfileSetupPresenter
 import de.hdodenhof.circleimageview.CircleImageView
+import java.io.ByteArrayOutputStream
 
 class RiderProfileSetup : AppCompatActivity(), IRiderProfileSetup {
 
@@ -19,12 +24,15 @@ class RiderProfileSetup : AppCompatActivity(), IRiderProfileSetup {
     private lateinit var imgUser: CircleImageView
     private lateinit var progressBar: ProgressBar
     private lateinit var profileSetupPresenter: IRiderProfileSetupPresenter
+    lateinit var bitmap: Bitmap
+    lateinit var stream: ByteArrayOutputStream
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rider_profile_setup)
         initViews()
-
+        getImage()
+        passListeners()
     }
 
     private fun initViews() {
@@ -34,6 +42,18 @@ class RiderProfileSetup : AppCompatActivity(), IRiderProfileSetup {
         imgUser = findViewById(R.id.img_rider_set_image)
         progressBar = findViewById(R.id.progress_rider_profile_setup)
         profileSetupPresenter = RiderProfileSetupPresenter(this, this@RiderProfileSetup)
+    }
+
+    private fun getImage() {
+        imgUser.setOnClickListener {
+            var intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            startActivityForResult(intent, 60)
+        }
+    }
+
+    private fun passListeners() {
+
     }
 
     override fun uploadResult(results: String) {
@@ -48,5 +68,13 @@ class RiderProfileSetup : AppCompatActivity(), IRiderProfileSetup {
     private fun hideProgress() {
         btnSave.isEnabled = true
         progressBar.visibility = View.INVISIBLE
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 69 && resultCode == Activity.RESULT_OK && data != null) {
+            bitmap = MediaStore.Images.Media.getBitmap(contentResolver, data?.data)
+            stream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 60, stream)
+        }
     }
 }
