ceb556bd6f4ab48d39b24e636b0bbfe4c37823f9
Faizan <mohammadfaizan980@gmail.com>
2019-01-08 16:58:48 +0500

    Added some kotlin code for rider profile setup

--- app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/RiderComponents/SetupProfile/RiderProfileSetup.kt
+++ app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/RiderComponents/SetupProfile/RiderProfileSetup.kt
@@ -1,10 +1,20 @@
 package com.whatsclone.muhammadfaizan.uberclone.RiderComponents.SetupProfile
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 import com.whatsclone.muhammadfaizan.uberclone.R
+import de.hdodenhof.circleimageview.CircleImageView
 
 class RiderProfileSetup : AppCompatActivity() {
+
+    private lateinit var edtUserName: EditText
+    private lateinit var edtuserPhone: EditText
+    private lateinit var btnSave: Button
+    private lateinit var imgUser: CircleImageView
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
