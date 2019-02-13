0bf4e31e43915f5d7964f916a52a023208430e5c
Faizan <mohammadfaizan980@gmail.com>
2019-02-13 12:28:36 +0500

    Added credential validation code for driver login

--- app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/DriverComponents/LoginRegister/View/DriverLogin/LoginView/LoginViewDriver.kt
+++ app/src/main/java/com/whatsclone/muhammadfaizan/uberclone/DriverComponents/LoginRegister/View/DriverLogin/LoginView/LoginViewDriver.kt
@@ -3,16 +3,35 @@ package com.whatsclone.muhammadfaizan.uberclone.DriverComponents.LoginRegister.V
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.whatsclone.muhammadfaizan.uberclone.DriverComponents.LoginRegister.View.DriverLogin.Contracts.Contract
+import com.whatsclone.muhammadfaizan.uberclone.DriverComponents.LoginRegister.View.DriverLogin.LoginPresenter.LoginPresenterDriver
 import com.whatsclone.muhammadfaizan.uberclone.R
 import com.whatsclone.muhammadfaizan.uberclone.WelcomeActivity.ActivityWelcome
 
 class LoginViewDriver : AppCompatActivity(), Contract.ILoginViewDriver {
+    private lateinit var btnLogin: Button
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPass: EditText
+    private lateinit var presenter: Contract.ILoginPresenterDriver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_view_driver)
+        initViews()
+    }
+
+    private fun initViews() {
+        btnLogin = findViewById(R.id.btn_login_driver)
+        edtEmail = findViewById(R.id.edt_login_email_driver)
+        edtPass = findViewById(R.id.edt_login_password_driver)
+        presenter = LoginPresenterDriver(this@LoginViewDriver, this@LoginViewDriver)
+
+        btnLogin.setOnClickListener {
+            presenter.onLoginInitiated(edtEmail.text.toString(), edtPass.text.toString())
+        }
     }
 
     override fun onLoginResults(result: Boolean) {
